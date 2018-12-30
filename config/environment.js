c2e3b555c151649577aaf11695dc29a892ffcbcb
Mark Hendrickson <markmhendrickson@gmail.com>
2018-12-30 14:41:35 +0100

    Clean up config file

--- config/environment.js
+++ config/environment.js
@@ -4,25 +4,17 @@ require('park-ranger')();
 
 module.exports = function(environment) {
   let ENV = {
-    modulePrefix: 'humans',
+    APP: {},
     environment,
+    modulePrefix: 'humans',
     rootURL: '/',
     locationType: 'router-scroll',
     historySupportMiddleware: true,
     EmberENV: {
-      FEATURES: {
-        // Here you can enable experimental features on an ember canary build
-        // e.g. 'with-controller': true
-      },
+      FEATURES: {},
       EXTEND_PROTOTYPES: {
-        // Prevent Ember Data from overriding Date.parse.
         Date: false
       }
-    },
-
-    APP: {
-      // Here you can pass flags/options to your application instance
-      // when it is created
     },
     host: {
       ipAddress: process.env.hostIpAddress,
@@ -30,28 +22,12 @@ module.exports = function(environment) {
     }
   };
 
-  if (environment === 'development') {
-    // ENV.APP.LOG_RESOLVER = true;
-    // ENV.APP.LOG_ACTIVE_GENERATION = true;
-    // ENV.APP.LOG_TRANSITIONS = true;
-    // ENV.APP.LOG_TRANSITIONS_INTERNAL = true;
-    // ENV.APP.LOG_VIEW_LOOKUPS = true;
-  }
-
   if (environment === 'test') {
-    // Testem prefers this...
     ENV.locationType = 'none';
-
-    // keep test console output quieter
     ENV.APP.LOG_ACTIVE_GENERATION = false;
     ENV.APP.LOG_VIEW_LOOKUPS = false;
-
     ENV.APP.rootElement = '#ember-testing';
     ENV.APP.autoboot = false;
-  }
-
-  if (environment === 'production') {
-
   }
 
   return ENV;
