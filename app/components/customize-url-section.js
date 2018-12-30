2109792df4b720bd50cf11aea6d13d22e5299064
Mark Hendrickson <markmhendrickson@gmail.com>
2018-12-30 14:20:49 +0100

    Format customize URL route responsively

--- app/components/customize-url-section.js
+++ app/components/customize-url-section.js
@@ -1,7 +1,7 @@
 import Component from '@ember/component';
 
 export default Component.extend({
-  classNames: ['customize-url-section', 'page'],
+  classNames: ['customize-url-section'],
   tagName: 'section',
   ipAddress: '127.0.0.1',
   domain: 'humansapp.io'
