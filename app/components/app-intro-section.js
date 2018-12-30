e7e7a991cde9b471dd0140268c8c66ba94827f33
Mark Hendrickson <markmhendrickson@gmail.com>
2018-12-30 15:41:40 +0100

    Show images upon lading

--- app/components/app-intro-section.js
+++ app/components/app-intro-section.js
@@ -1,6 +1,19 @@
 import Component from '@ember/component';
+import { computed } from '@ember/object';
 
 export default Component.extend({
+  classNameBindings: ['showImages'],
   classNames: ['app-intro', 'hero'],
-  tagName: 'section'
+  tagName: 'section',
+  imagesLoaded: 0,
+
+  didInsertElement() {
+    this.$('img').on('load', () => {
+      this.incrementProperty('imagesLoaded');
+    });
+  },
+
+  showImages: computed('imagesLoaded', function() {
+    return (this.get('imagesLoaded') == 2);
+  })
 });
--- app/components/blockstack-intro-section.js
+++ app/components/blockstack-intro-section.js
@@ -1,10 +1,23 @@
 import Component from '@ember/component';
 import { inject as service } from '@ember/service';
+import { computed } from '@ember/object';
 
 export default Component.extend({
+  classNameBindings: ['showImages'],
   classNames: ['blockstack-intro', 'hero'],
   session: service(),
   tagName: 'section',
+  imagesLoaded: 0,
+
+  didInsertElement() {
+    this.$('img').on('load', () => {
+      this.incrementProperty('imagesLoaded');
+    });
+  },
+
+  showImages: computed('imagesLoaded', function() {
+    return (this.get('imagesLoaded') == 2);
+  }),
 
   actions: {
     authenticate() {
