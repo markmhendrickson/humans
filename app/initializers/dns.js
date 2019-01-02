969aca1402fd4350f4719e4a6f99f95c9e2c293e
Mark Hendrickson <markmhendrickson@gmail.com>
2019-01-02 23:33:07 +0100

    Improve DNS initializer

--- app/initializers/dns.js
+++ app/initializers/dns.js
@@ -2,11 +2,11 @@ import $ from 'jquery';
 import config from 'humans/config/environment';
 
 export function initialize(app) {
-  if (!window) { return; }
+  if (!window || window.location.hostname === config.location.hostname) { return; }
 
   app.deferReadiness();
 
-  $.getJSON(`https://dns.google.com/resolve?name=${window.location.origin}&type=TXT`).then((data) => {
+  $.getJSON(`https://dns.google.com/resolve?name=${window.location.hostname}&type=TXT`).then((data) => {
     if (data && data.Answer && data.Answer[0].data && data.Answer[0].data.indexOf('blockstack=') === 1) {
       config.blockstackName = data.Answer[0].data.replace(/"/g, '').substr(11);
     }
